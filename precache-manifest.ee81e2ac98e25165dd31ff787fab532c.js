4706a6689b7daa1a1386002466e96d8ba08278b5
David Bautista <dj.bautista10@uniandes.edu.co>
2020-09-01 14:04:01 -0500

    Updates

---
+++ precache-manifest.ee81e2ac98e25165dd31ff787fab532c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293d0b5b779a5ae33c9fcaedf419e4b1",
+    "url": "/innovation-map/index.html"
+  },
+  {
+    "revision": "e5fda09ef5701b752e6b",
+    "url": "/innovation-map/static/css/4.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c5ddfb55e22f21a921d3",
+    "url": "/innovation-map/static/css/main.1a0420ad.chunk.css"
+  },
+  {
+    "revision": "933e1da9ee808c3fda93",
+    "url": "/innovation-map/static/js/3.ecae8629.chunk.js"
+  },
+  {
+    "revision": "e5fda09ef5701b752e6b",
+    "url": "/innovation-map/static/js/4.1c62ec61.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/innovation-map/static/js/4.1c62ec61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42034c0c3516a8eca71",
+    "url": "/innovation-map/static/js/5.9a0c3e84.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/innovation-map/static/js/5.9a0c3e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c18c53d682acb22483b",
+    "url": "/innovation-map/static/js/6.2c5762f2.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/innovation-map/static/js/6.2c5762f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ddfb55e22f21a921d3",
+    "url": "/innovation-map/static/js/main.8e5ac495.chunk.js"
+  },
+  {
+    "revision": "ff22263bd70fbb5f0151",
+    "url": "/innovation-map/static/js/runtime-main.25de98e3.js"
+  },
+  {
+    "revision": "ac36f644ac8adb47d6c6",
+    "url": "/innovation-map/static/js/xlsx.22b8c245.chunk.js"
+  }
+]);
